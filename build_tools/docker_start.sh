6e5570e307948bb559842ebd7cbcebc5519eebe0
creworkg <creative.work.g@gmail.com>
2022-10-17 08:11:17 -0400

    chore:add flash tools

--- build_tools/docker_start.sh
+++ build_tools/docker_start.sh
@@ -5,6 +5,7 @@ docker run --rm \
     --name=armf \
     --net=host \
     -v /home/g/arm_ws:/home/arm/ws \
+    -v /media/sf_IMX6ULL:/home/arm/imx6ull \
     -w /home/arm/ws \
     arm-linux-dev:jelly \
     bash
